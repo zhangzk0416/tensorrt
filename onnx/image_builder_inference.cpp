fa84ee31711a2fe5a120de271a6e6ff0f0678151
zhangzk0416 <137976147+zhangzk0416@users.noreply.github.com>
2025-04-11 10:14:21 +0800

    Update image_builder_inference.cpp

--- onnx/image_builder_inference.cpp
+++ onnx/image_builder_inference.cpp
@@ -15,12 +15,12 @@
 #include <vector>
 #include <memory>
 #include <functional>
-#include <unistd.h>
 #include <chrono>
 #include <shlwapi.h>
 #include <opencv2/opencv.hpp>
 
 using namespace std;
+#pragma   comment(lib,"shlwapi.lib") 
 
 #define checkRuntime(op)  __check_cuda_runtime((op), #op, __FILE__, __LINE__)
 
@@ -49,16 +49,6 @@ class TRTLogger : public nvinfer1::ILogger {
 public:
 	virtual void log(Severity severity, nvinfer1::AsciiChar const* msg) noexcept override {
 		if (severity <= Severity::kINFO) {
-			// 鎵撳嵃甯﹂鑹茬殑瀛楃锛屾牸寮忓涓嬶細
-			// printf("\033[47;33m鎵撳嵃鐨勬枃鏈琝033[0m");
-			// 鍏朵腑 \033[ 鏄捣濮嬫爣璁?
-			//      47    鏄儗鏅鑹?
-			//      ;     鍒嗛殧绗?
-			//      33    鏂囧瓧棰滆壊
-			//      m     寮€濮嬫爣璁扮粨鏉?
-			//      \033[0m 鏄粓姝㈡爣璁?
-			// 鍏朵腑鑳屾櫙棰滆壊鎴栬€呮枃瀛楅鑹插彲涓嶅啓
-			// 閮ㄥ垎棰滆壊浠ｇ爜 https://blog.csdn.net/ericbar/article/details/79652086
 			if (severity == Severity::kWARNING) {
 				printf("\033[33m%s: %s\033[0m\n", severity_string(severity), msg);
 			}
@@ -89,6 +79,19 @@ bool exists(const string& path) {
 }
 
 bool build_model() {
+	/*
+	1. 创建builder
+	2. 创建config
+	3. 创建network
+	4. 创建parser，加载网络
+	5. 添加配置
+		添加最大空间
+		创建profile，配置profile动态尺寸
+		profile加入config
+	6. 创建engine
+	7. 序列化engine
+	8. 保存文件
+	*/
 
 	if (exists("engine.trtmodel")) {
 		printf("Engine.trtmodel has exists.\n");
@@ -97,14 +100,14 @@ bool build_model() {
 
 	TRTLogger logger;
 
-	
+
 	auto builder = make_nvshared(nvinfer1::createInferBuilder(logger));
 	auto config = make_nvshared(builder->createBuilderConfig());
 	auto network = make_nvshared(builder->createNetworkV2(1));
 
-	
+
 	auto parser = make_nvshared(nvonnxparser::createParser(*network, logger));
-	if (!parser->parseFromFile("classifier.onnx", 1)) {
+	if (!parser->parseFromFile("F:/tensorrt-integrate/1.1-full-cnn-classifier/workspace/classifier.onnx", 1)) {
 		printf("Failed to parse classifier.onnx\n");
 		return false;
 	}
@@ -131,9 +134,9 @@ bool build_model() {
 	}
 
 	auto model_data = make_nvshared(engine->serialize());
-	FILE* f = fopen("engine.trtmodel", "wb");
-	fwrite(model_data->data(), 1, model_data->size(), f);
-	fclose(f);
+	std::ofstream outfile("F:/and/mlp.engine", std::ios::binary);
+	cout << model_data->size() << endl;
+	outfile.write((char *)model_data->data(), model_data->size());
 
 	printf("Done.\n");
 	return true;
@@ -179,7 +182,7 @@ vector<string> load_labels(const char* file) {
 void inference() {
 
 	TRTLogger logger;
-	auto engine_data = load_file("engine.trtmodel");
+	auto engine_data = load_file("F:/and/mlp.engine");
 	auto runtime = make_nvshared(nvinfer1::createInferRuntime(logger));
 	auto engine = make_nvshared(runtime->deserializeCudaEngine(engine_data.data(), engine_data.size()));
 	if (engine == nullptr) {
@@ -203,7 +206,7 @@ void inference() {
 	checkRuntime(cudaMalloc(&input_data_device, input_numel * sizeof(float)));
 
 	// image to float
-	auto image = cv::imread("dog.jpg");
+	auto image = cv::imread("F:/tensorrt-integrate/1.1-full-cnn-classifier/workspace/dog.jpg");
 	float mean[] = { 0.406, 0.456, 0.485 };
 	float std[] = { 0.225, 0.224, 0.229 };
 
@@ -219,7 +222,7 @@ void inference() {
 	for (int i = 0; i < image_area; ++i, pimage += 3) {
 		// 进行标准化操作（pixel/255.0-mean）/std    
 		// 同时实现bgr->rgb   这种方式比通过cvtcolor转换快
-		*phost_r++ = (pimage[0] -55.0f - mean[0]) / std[0];
+		*phost_r++ = (pimage[0] - 55.0f - mean[0]) / std[0];
 		*phost_g++ = (pimage[1] / 255.0f - mean[1]) / std[1];
 		*phost_b++ = (pimage[2] / 255.0f - mean[2]) / std[2];
 	}
@@ -244,10 +247,10 @@ void inference() {
 	checkRuntime(cudaStreamSynchronize(stream));
 
 	float* prob = output_data_host;
-	int predict_label = std::max_element(prob, prob + num_classes) - prob;  
-	auto labels = load_labels("labels.imagenet.txt");
+	int predict_label = std::max_element(prob, prob + num_classes) - prob;
+	auto labels = load_labels("F:/tensorrt-integrate/1.1-full-cnn-classifier/workspace/labels.imagenet.txt");
 	auto predict_name = labels[predict_label];
-	float confidence = prob[predict_label];    
+	float confidence = prob[predict_label];
 	printf("Predict: %s, confidence = %f, label = %d\n", predict_name.c_str(), confidence, predict_label);
 
 	checkRuntime(cudaStreamDestroy(stream));
